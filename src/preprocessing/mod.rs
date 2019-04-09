201fa3b9d0aac49595f3dd69b343f6a19dc18f18
Michael Bausano <michael@joinpouch.com>
2019-04-09 07:47:25 +0100

    heat map

--- src/preprocessing/mod.rs
+++ src/preprocessing/mod.rs
@@ -10,7 +10,7 @@ use self::image::{Luma, ImageBuffer, GenericImageView};
 use self::image::imageops::colorops::invert;
 
 pub fn start_data_channel() {
-  let file = "output_0331";
+  let file = "output_0389";
   let image = image::open("data/debug-1/video/".to_owned() + file.clone() + ".png").unwrap();
 
   let (width, height) = image.dimensions();
@@ -19,15 +19,15 @@ pub fn start_data_channel() {
   let mut edge_detector = ImageBuffer::new(width, height);
 
   let horizontal_edges = filter3x3(&grayscale(&image), &[
-    10_f32, 10_f32, 10_f32,
+    7.5_f32, 7.5_f32, 7.5_f32,
     1_f32, 1_f32, 1_f32,
-    -10_f32, -10_f32, -10_f32,
+    -7.5_f32, -7.5_f32, -7.5_f32,
   ]);
 
   let vertical_edges = filter3x3(&grayscale(&image), &[
-    10_f32, 1_f32, -10_f32,
-    10_f32, 1_f32, -10_f32,
-    10_f32, 1_f32, -10_f32,
+    7.5_f32, 1_f32, -7.5_f32,
+    7.5_f32, 1_f32, -7.5_f32,
+    7.5_f32, 1_f32, -7.5_f32,
   ]);
 
   for (x, y, pixel) in edge_detector.enumerate_pixels_mut() {
@@ -45,24 +45,23 @@ pub fn start_data_channel() {
 
   edge_detector.save("output/test/".to_owned() + file + "_edge.png").unwrap();
 
-  let cell_size = 20;
-  let heat_map_cols = height / cell_size;
-  let heat_map_rows = width / cell_size;
+  let cell_size = 40;
+  let heat_map_cols = (2 * width / cell_size) - 1;
+  let heat_map_rows = (2 * height / cell_size) - 1;
 
   let mut heat_map: Vec<Vec<u32>> = Vec::new();
-  let mut heat_max: u32 = 0;
 
-  for offset_y in 0..heat_map_cols {
+  for offset_y in 0..heat_map_rows {
     let mut row: Vec<u32> = Vec::new();
 
-    for offset_x in 0..heat_map_rows {
+    for offset_x in 0..heat_map_cols {
       let mut heat: u32 = 0;
 
       for cell_y in 0..cell_size {
         for cell_x in 0..cell_size {
           let pixel = edge_detector.get_pixel(
-            (offset_x * cell_size) + cell_x,
-            (offset_y * cell_size) + cell_y,
+            (offset_x * cell_size / 2) + cell_x,
+            (offset_y * cell_size / 2) + cell_y,
           );
 
           if pixel.data[0] == 0 {
@@ -71,20 +70,51 @@ pub fn start_data_channel() {
         }
       }
 
-      heat_max = heat_max.max(heat.clone());
-
       row.push(heat);
     }
 
     heat_map.push(row);
   }
 
+  let heat_map_cols = heat_map_cols + 1;
+  let heat_map_rows = heat_map_rows + 1;
+  let mut final_heat_map: Vec<Vec<u32>> = Vec::new();
+  let mut heat_max: u32 = 1;
+
+  for offset_y in 0..heat_map_rows {
+    let mut row: Vec<u32> = vec!(0);
+
+    for offset_x in 1..(heat_map_cols - 1) {
+      if offset_y == 0 || offset_y == heat_map_rows - 1 {
+        row.push(0);
+        continue;
+      }
+
+      let mut heat: u32 = 0;
+      heat += heat_map[offset_y as usize][offset_x as usize];
+      heat += heat_map[offset_y as usize][(offset_x - 1) as usize];
+      heat += heat_map[(offset_y - 1) as usize][offset_x as usize];
+      heat += heat_map[(offset_y - 1) as usize][(offset_x - 1) as usize];
+      let heat: u32 = heat / 4;
+
+      heat_max = heat_max.max(heat.clone());
+
+      row.push(heat);
+    }
+
+    row.push(0);
+
+    final_heat_map.push(row);
+  }
+
   let unit: f64 = 255_f64 / heat_max as f64;
 
   for (x, y, pixel) in heat_detector.enumerate_pixels_mut() {
-    let col = (x / cell_size) as usize;
-    let row = (y / cell_size) as usize;
-    let mut heat: u8 = (unit * heat_map[row][col] as f64) as u8;
+    let col = (x / (cell_size / 2)) as usize;
+    let row = (y / (cell_size / 2)) as usize;
+    let heat = final_heat_map[row][col];
+
+    let mut heat: u8 = (unit * heat as f64) as u8;
 
     *pixel = Luma([heat]);
   }
