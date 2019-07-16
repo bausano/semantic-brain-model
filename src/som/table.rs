858e3901df15a4d47ba0fae430a099c79bf7a61e
Michael Bausano <michael@joinpouch.com>
2019-07-16 20:17:30 +0100

    WIP

--- src/som/table.rs
+++ src/som/table.rs
@@ -2,22 +2,22 @@ use super::neuron::Neuron;
 use super::point::Point;
 use std::rc::Rc;
 
-pub struct Table {
-    /// Midpoint that splits the table into 4 quadrants.
-    split: Point,
+pub enum Table {
+    /// Leaf only needs to know where is it in the space and what neuron it represents.
+    Leaf(Point, usize),
+    Node {
+        /// Midpoint that splits the table into 4 quadrants.
+        split: Point,
 
-    /// Each table is split into four quadrants.
-    quadrants: [Option<Rc<Table>>; 4],
+        /// Each table is split into four quadrants.
+        quadrants: [Option<Rc<Table>>; 4],
 
-    /// Whether the table is limited by a bottom left point.
-    lower_bound: Option<Point>,
+        /// Whether the table is limited by a bottom left point.
+        lower_bound: Option<Point>,
 
-    /// Whether the table is limited by a top right point.
-    upper_bound: Option<Point>,
-
-    /// Whether there is a neuron connected to the table. Table can have either
-    /// quadrants or neuron, not both.
-    neuron: Option<usize>,
+        /// Whether the table is limited by a top right point.
+        upper_bound: Option<Point>,
+    },
 }
 
 impl Table {
@@ -32,7 +32,34 @@ impl Table {
         }
     }
 
-    pub fn insert(&mut self, neuron: usize, at: Point) {}
+    pub fn insert(&mut self, neuron: usize, at: Point) {
+        let quadrant = which_quadrant(self.split, at);
+
+        if let Some(neuron) = self.neuron {
+            self.neuron = None;
+
+            return;
+        }
+
+        let boxed: &Option<Rc<Table>> = &self.quadrants[quadrant];
+
+        match boxed {
+            Some(cell) => cell.insert(neuron, at),
+            None => {
+                let mut quadrant_table = Table::new(at);
+                quadrant_table.neuron = Some(neuron);
+            }
+        }
+
+        // if neuron already exists
+        //   create two new tables with both new and old neurons
+        //   remove neuron from this table
+        //   push new tables into this table with appropriate bounds
+        // else if quadrant table doesnt exist
+        //   create new table with the neuron and assign it as the quadrant
+        // else
+        //   calt insert on the quadrant table
+    }
 
     /// Retrieves neuron at given point if exists.
     pub fn at(&self, point: Point) -> Option<usize> {
@@ -46,33 +73,13 @@ impl Table {
             };
         }
 
-        // Decides which quadrant should the point belong to.
-        //
-        //   point.y >= table.y   | point.y >= table.y
-        //   point.x < table.x    | point.x >= table.x
-        //  ----------------------+--------------------
-        //   point.y < table.y    | point.y < table.y
-        //   point.x <= table.x   | point.x > table.x
-        //
-        let quadrant = if point.y >= self.split.y {
-            if point.x >= self.split.x {
-                0
-            } else {
-                1
-            }
-        } else {
-            if point.x <= self.split.x {
-                2
-            } else {
-                3
-            }
-        };
+        let quadrant = which_quadrant(self.split, point);
 
         let boxed: &Option<Rc<Table>> = &self.quadrants[quadrant];
 
         match boxed {
             Some(cell) => cell.at(point),
-            _ => None,
+            None => None,
         }
     }
 
@@ -80,4 +87,27 @@ impl Table {
     pub fn around(&self, center: Point, radius: f64) -> Option<usize> {
         None
     }
+}
+
+/// Decides to which quadrant does a point belong to with respect to a center point.
+///
+///   point.y >= table.y   | point.y >= table.y
+///   point.x < table.x    | point.x >= table.x
+///  ----------------------+--------------------
+///   point.y < table.y    | point.y < table.y
+///   point.x <= table.x   | point.x > table.x
+fn which_quadrant(center: Point, point: Point) -> usize {
+    if point.y >= self.split.y {
+        if point.x >= self.split.x {
+            0
+        } else {
+            1
+        }
+    } else {
+        if point.x <= self.split.x {
+            2
+        } else {
+            3
+        }
+    };
 }
